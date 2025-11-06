ff7133c6f0e3d6875e2513988efaefdd0c2ba26e
ganesh-badar <149141995+ganesh-badar@users.noreply.github.com>
2025-11-07 00:47:39 +0530

    Add files via upload

---
+++ Exception/Demo1.java
@@ -0,0 +1,14 @@
+package Exception;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		int arr[]= {10,20,30,40,50,60};
+		System.out.println(arr[0]);
+		System.out.println(arr[-1]);
+		System.out.println("Hello");
+		System.out.println(arr[2]);
+
+	}
+
+}
