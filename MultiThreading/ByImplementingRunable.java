05fecdf4e13fafeb37034536cf01c32d7319447e
ganesh-badar <149141995+ganesh-badar@users.noreply.github.com>
2025-11-08 16:00:18 +0530

    Add files via upload

---
+++ MultiThreading/ByImplementingRunable.java
@@ -0,0 +1,22 @@
+package MultiThreading;
+
+
+
+
+class Mthread1 implements Runnable{
+	
+	public void run() {
+		System.out.println("In run() method");
+	}
+			
+}
+public class ByImplementingRunable {
+
+	public static void main(String[] args) {
+		Mthread1 m = new Mthread1();
+		Thread t = new Thread(m);
+		t.start();
+
+	}
+
+}
