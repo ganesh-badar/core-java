b8f1562d5ffb4f544b8cc709923d6cf14f8c86d5
ganesh-badar <149141995+ganesh-badar@users.noreply.github.com>
2025-11-08 15:48:55 +0530

    Add files via upload

---
+++ MultiThreading/ByextendThread.java
@@ -0,0 +1,18 @@
+package MultiThreading;
+
+class Mthread  extends Thread{
+	@Override
+	public void run() {
+		System.out.println(" In (run() Method ");
+	}
+	  
+	  
+   }
+public class ByextendThread {
+	public static void main(String[] args) {
+		Mthread m = new Mthread();
+		m.start();
+
+	}
+
+}
